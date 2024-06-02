e394dd993ab3c3610ddedf21012c7ba3468a1dc1
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-06-02 14:43:55 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,10 +1,11 @@
-// sample.groovy
+def message(data) {
+    def name = data.name ?: "World" // Default to "World" if name is not in the JSON
+    def greetMessage = greet(name)
+    println(greetMessage)
+}
 
 def greet(name) {
     return "Hello, $name!"
 }
-
-def message = greet("World")
-println(message)
 
 return this
