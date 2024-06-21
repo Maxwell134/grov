00b76e421e42694b9b51a217c9127158c2cece50
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-06-21 18:02:00 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,24 +1,19 @@
 import groovy.json.JsonSlurper
 
-class PipelineConfig {
-    def config
-
-    PipelineConfig(String jsonFilePath) {
-        def jsonSlurper = new JsonSlurper()
-        def jsonFile = new File(jsonFilePath)
-        if (jsonFile.exists()) {
-            config = jsonSlurper.parse(jsonFile)
-        } else {
-            throw new FileNotFoundException("File not found: ${jsonFilePath}")
-        }
-    }
-
-    def getConfig() {
-        return config
-    }
-}
-
 def loadConfig() {
-    def pipelineConfig = new PipelineConfig('pipeline.json')
-    return pipelineConfig.getConfig()
+    def jsonSlurper = new JsonSlurper()
+    def jsonFile = new File('pipeline.json')
+    if (jsonFile.exists()) {
+        def config = jsonSlurper.parse(jsonFile)
+        def environment = config.environment
+        def version = config.version
+                    
+        echo "Environment: ${environment}"
+        echo "Version: ${version}"
+        
+    } else {
+        throw new FileNotFoundException("File not found: pipeline.json")
+    }
 }
+
+return this 
